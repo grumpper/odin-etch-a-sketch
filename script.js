64c8d3aa620e9de6a88e00accbae95acf617a13a
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-02-05 16:21:12 +0200

    add grid setup button

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 // draw the grid
-const gridSize = 16
-const container = document.querySelector('#container')
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div')
-    row.classList.add("row")
-    for (let j = 0; j < gridSize; j++) {
-        const square = document.createElement('div')
-        square.classList.add("square")
-        row.appendChild(square)
+function drawGrid(size) {
+    const container = document.querySelector('#container')
+    container.replaceChildren()
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div')
+        row.classList.add("row")
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement('div')
+            square.classList.add("square")
+            row.appendChild(square)
+        }
+        container.appendChild(row)
     }
-    container.appendChild(row)
+    // add color on hover
+    let squares = document.querySelectorAll('div.square')
+    squares.forEach((e) => {
+        e.addEventListener("mouseover", () => e.style.background = "blue")
+    })
 }
 
-// add color on hover
-const squares = document.querySelectorAll('div.square')
-squares.forEach((e) => {
-    e.addEventListener("mouseover", () => e.style.background = "blue")
+// setup button
+const button = document.querySelector('#setup-btn')
+let gridSize = 16
+drawGrid(gridSize)
+button.addEventListener('click', () => {
+    do {
+        gridSize = prompt("How many square per row should be the grid?")
+    } while (gridSize < 16 && gridSize > 100)
+    drawGrid(gridSize)
 })
